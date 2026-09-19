a7adb93c5ef59b717990123ab52fd6e00b6e78e8
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 10:59:28 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878746-last-digit-of-number-GFG/878746-last-digit-of-number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        int rem = n%10;
+        if(rem<0){
+            rem = rem * -1;
+        }
+        System.out.println(rem);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
