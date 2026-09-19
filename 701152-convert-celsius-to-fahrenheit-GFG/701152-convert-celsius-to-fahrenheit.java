b4647f615d5c1d07ad99329529f8c974811ac25d
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 11:00:04 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 701152-convert-celsius-to-fahrenheit-GFG/701152-convert-celsius-to-fahrenheit.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double cToF(int C) {
+        // code here
+        return ((C*9/5)+32);
+
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
