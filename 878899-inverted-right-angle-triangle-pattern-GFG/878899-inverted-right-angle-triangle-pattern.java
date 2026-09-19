14085b6900552ed1fe61794fcab59d22c703e8dd
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 14:32:57 +0530

    Added solution - Time: O(n^2), Space: O(1)

---
+++ 878899-inverted-right-angle-triangle-pattern-GFG/878899-inverted-right-angle-triangle-pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int row = n; row >= 1; row--){
+            for(int col = 1; col <= row; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
