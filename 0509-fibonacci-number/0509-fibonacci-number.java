5c3defee7f43ed541d5e97f17ea33c98a4c3ca11
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 11:55:14 +0530

    Time: 9 ms (41.82%), Space: 42 MB (70.31%) - LeetHub - Time: O(2^n), Space: O(n)

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+        else{
+            return fib(n-1) + fib(n-2);
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
