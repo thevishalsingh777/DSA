adfd14d916086c8c5a8788cc1eda5b40df73f382
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 15:10:01 +0530

    Added solution - Time: O(n^2), Space: O(1)

---
+++ 705563-inverted-triangle-of-stars-GFG/705563-inverted-triangle-of-stars.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        // code here
+        for(int row = 1; row <= n; row++){
+            for(int col = 0; col < row-1; col++){
+                System.out.print(" ");
+            }
+            for(int k = 1;k <=(n-row)*2+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
