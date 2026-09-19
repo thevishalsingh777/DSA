2f2518df8e3dd748f4a2ac00f9408cb56399f236
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 10:41:31 +0530

    Time: 0 ms (100%), Space: 41.6 MB (98.68%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2 == 0){
+            return n;
+        }
+        else{
+            return n * 2;
+        }
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
