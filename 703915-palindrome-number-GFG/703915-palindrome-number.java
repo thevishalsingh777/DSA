9ff08d9bcbfd12cbb513adac3bdf988e4c26f7ba
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 10:56:53 +0530

    Added solution - Time: O(log₁₀(n)), Space: O(1)

---
+++ 703915-palindrome-number-GFG/703915-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+public boolean isPalindrome(int n) {
+int original = n;
+int rev = 0;
+while (n != 0) {
+int digit = n%10;
+rev = (rev*10) + digit;
+n = n/10;
+}
+return rev == original;
+}
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
