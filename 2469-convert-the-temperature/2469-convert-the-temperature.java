7a18e80a5b0f851fb8aad18b21c29e41a5568d9b
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-19 10:40:30 +0530

    Time: 0 ms (100%), Space: 46.1 MB (59.05%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+            return new double[] { celsius + 273.15, (celsius * 1.80) + 32.00};
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
