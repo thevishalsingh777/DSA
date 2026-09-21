4dff00ecab0d277d1b0c932301c96f98dc46098e
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-21 11:51:32 +0530

    Time: 3 ms (14.17%), Space: 44 MB (21.99%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2]) {
+            return "none";
+        } else if (nums[0] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
