324c8e84bfda2eb7c23d8c01c2e7fef32b056823
Vishal <121882028+thevishalsingh777@users.noreply.github.com>
2026-09-25 11:29:36 +0530

    Time: 1 ms (98.13%), Space: 42.6 MB (44.78%) - LeetHub - Time: O(1), Space: O(1)

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -2,9 +2,23 @@ class Solution {
     public int addDigits(int num) {
         if (num == 0)
             return 0;
-        if (num % 9 == 0)
+        else if (num % 9 == 0)
             return 9;
+        else
         return num % 9;
+        // if(num < 10) return num;
+        // while(num >= 10){
+        //     int sum = 0;
+        //     int val = num;
+        //     while(val!=0){
+        //         int rem = val%10;
+        //         sum+=rem;
+        //         val/=10;
+        //     }
+        //     num = sum;
+        // }
+        // return num;
+
     }
 }
 
